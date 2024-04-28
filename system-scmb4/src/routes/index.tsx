ed07658cc3447b44e1948df2352a734288779e0b
maria-geyzianny <sgeizianny@gmail.com>
2024-04-28 15:09:01 -0300

    feat: adicionado tema escuro e a troca do tema claro

--- system-scmb4/src/routes/index.tsx
+++ system-scmb4/src/routes/index.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
   return (
     <Routes>
       <Route
         path="/cadastro-projetos"
         element={
-          <Button variant="contained" color="primary">
-            Teste
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Alterando o tema
           </Button>
         }
       />
