7bf26459930119c0f844cca84b5e5942aa03d2d6
maria-geyzianny <sgeizianny@gmail.com>
2024-04-28 12:56:36 -0300

    feat: Adicionando MUI 6

--- system-scmb4/src/routes/index.js
+++ system-scmb4/src/routes/index.js
@@ -1,12 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route
-        path="/cadastro-projetos"
-        element={<p> Página de Cadastro de Projetos</p>}
-      />
+      <Route path="/cadastro-projetos" element={<Button>Teste</Button>} />
       <Route path="*" element={<Navigate to="/cadastro-projetos" />} />
     </Routes>
   );
