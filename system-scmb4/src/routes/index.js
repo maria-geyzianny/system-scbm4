2b911f45d0dcd1788dcd72e8a5fc9030d73808c6
maria-geyzianny <sgeizianny@gmail.com>
2024-04-28 12:39:15 -0300

    feat: Adicionando react-router-dom

--- system-scmb4/src/routes/index.js
+++ system-scmb4/src/routes/index.js
@@ -1,1 +1,13 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/cadastro-projetos"
+        element={<p> Página de Cadastro de Projetos</p>}
+      />
+      <Route path="*" element={<Navigate to="/cadastro-projetos" />} />
+    </Routes>
+  );
+};
