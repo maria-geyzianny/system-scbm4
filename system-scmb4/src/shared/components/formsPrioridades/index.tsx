697a6a2f13514e57466345ece2888177c6c39972
maria-geyzianny <sgeizianny@gmail.com>
2024-05-26 17:56:25 -0300

    feature[formulario de cadastro de projeto]: add dropdown em etapas do projeto

--- system-scmb4/src/shared/components/formsPrioridades/index.tsx
+++ system-scmb4/src/shared/components/formsPrioridades/index.tsx
@@ -49,12 +49,12 @@ const categories = [
     ],
   },
 ];
-type PriorityState = Record<string, number>;
+type PriorityState = Record<string, string>;
 
 const ProjectPage = () => {
   const [priorities, setPriorities] = useState<PriorityState>({});
 
-  const handlePriorityChange = (category: string, priority: number) => {
+  const handlePriorityChange = (category: string, priority: string) => {
     setPriorities((prevState) => {
       const updatedPriorities = { ...prevState };
       updatedPriorities[category] = priority;
@@ -63,25 +63,49 @@ const ProjectPage = () => {
   };
 
   const handleSubmit = () => {
-    // Handle submission of priorities
     console.log("Priorities:", priorities);
   };
 
   return (
-    <div>
-      <Typography variant="h4" gutterBottom>
-        Organize em ordem de importância os atributos importantes para ESTE
-        projeto
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <Typography
+        style={{
+          fontSize: "2rem",
+          fontFamily: "serif",
+          color: "#4d4b4b",
+          marginLeft: "-70%",
+        }}
+      >
+        Organize em ordem de importância os atributos importantes para o projeto
       </Typography>
-      <Grid container spacing={2}>
+      <Grid
+        container
+        spacing={2}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "1rem",
+          width: "150%",
+          marginTop: "50px",
+          marginRight: "20%",
+        }}
+      >
         {categories.map((category, index) => (
-          <Grid item xs={12} sm={6} key={index}>
+          <Grid item xs={12} sm={6} md={4} key={index}>
             <FormControl fullWidth>
               <InputLabel>{category.title}</InputLabel>
               <Select
                 value={priorities[category.title] || ""}
                 onChange={(e) =>
-                  handlePriorityChange(category.title, Number(e.target.value))
+                  handlePriorityChange(category.title, e.target.value)
                 }
               >
                 <MenuItem value="">Selecione...</MenuItem>
@@ -95,19 +119,26 @@ const ProjectPage = () => {
           </Grid>
         ))}
       </Grid>
-      <Button variant="contained" color="primary" onClick={handleSubmit}>
-        Enviar Prioridades
-      </Button>
-      <Typography
-        variant="subtitle1"
-        gutterBottom
+      <Grid
+        container
+        spacing={3}
+        justifyContent="center"
         style={{ marginTop: "20px" }}
       >
+        <Grid item>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Enviar Prioridades
+          </Button>
+        </Grid>
+      </Grid>
+      <Typography variant="subtitle1" gutterBottom align="left">
         Ranking de Prioridades:
       </Typography>
       <ol>
-        {Object.values(priorities).map((priority, index) => (
-          <li key={index}>{priority}</li>
+        {Object.entries(priorities).map(([category, priority], index) => (
+          <li key={index}>
+            <strong>{category}:</strong> {priority}
+          </li>
         ))}
       </ol>
     </div>
